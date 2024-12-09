f9592a44cef21eed4f5ed2e2cda683d010e15bcc
Jiajing <lijiajing2003@gmail.com>
2024-12-09 10:00:39 +0100

    Qestion 4

---
+++ enseash_Q4.c
@@ -0,0 +1,89 @@
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+    char command[128]; // Buffer for user command 
+    char prompt[128];  // Buffer for the dynamic prompt
+    int last_status = 0; // Variable to store the status of the last command
+
+    // Definition of constants that are the messages we want to display
+    const char *welcome_message = "Welcome to ENSEA Tiny Shell.\n";
+    const char *exit_message = "Type 'exit' to quit.\n";
+    const char *prompt_message = "enseash % ";
+    const char *goodbye_message="Bye Bye.\n";
+    const char *prompt_exit_code= "enseash [exit:%d] %% ";
+	const char *prompt_signal_code= "enseash [sign:%d] %% ";
+	
+
+
+    // Display of the welcome message thanks to write(STDOUT_FILENO,...)
+    write(STDOUT_FILENO, welcome_message, strlen(welcome_message));
+    write(STDOUT_FILENO, exit_message, strlen(exit_message));
+
+    while (1)
+	 {
+		  // Update the prompt based on the last status
+        if (WIFEXITED(last_status)) {
+            // If the last command exited normally, use the exit code
+            snprintf(prompt, sizeof(prompt), prompt_exit_code, WEXITSTATUS(last_status));
+        } 
+        else if (WIFSIGNALED(last_status)) {
+            // If the last command was terminated by a signal, use the signal number
+            snprintf(prompt, sizeof(prompt), prompt_signal_code, WTERMSIG(last_status));
+        } 
+        else {
+            // Default prompt if no command has been executed yet
+            strncpy(prompt, prompt_message, sizeof(prompt));
+        }
+	
+      // Display the prompt
+        write(STDOUT_FILENO, prompt, strlen(prompt));
+           	
+	 // Reading of the command written by the user thanks to read(STDIN_FILENO,...)
+        ssize_t n = read(STDIN_FILENO, command, sizeof(command) - 1);
+
+        // Handle Ctrl+D or error
+        if (n == 0 || n == -1) {
+            break;
+        }
+
+     // Remove the newline character from the command (to really get exit without the /n when we write exit )
+        command[n - 1] = '\0';
+
+     // Exit if user write "exit"
+        if (strcmp(command, "exit") == 0) {
+		
+            break;
+		}
+	// Create a child process to execute the command
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            continue;
+        }
+		if (pid == 0) {
+            // Code for the child process 
+            execlp(command, command, NULL);
+            // If execlp fails (if the command isn't installed)
+            perror("execlp");
+            exit(EXIT_FAILURE);
+       		 } 
+        else {
+            // Code for the parent process
+            int status;
+            wait(&status); // Wait for the child process to finish
+            
+            // Save the child's status for use in the next prompt
+            last_status = status;
+        	}
+
+       	}
+	//Good bye message when we exit
+	write(STDOUT_FILENO, goodbye_message, strlen(goodbye_message));
+	return 0;
+}
